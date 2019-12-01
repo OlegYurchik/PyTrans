52d6e4177eb17718667aafa1f636e15b0f55739c
Oleg Yurchik <oleg.yurchik@protonmail.com>
2019-12-01 20:14:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ import pyperclip
 from translation import get_translate
 import sys
 
-cd = pyperclip
+if __name__ == "__main__":
+    cd = pyperclip
+    to_lang = sys.argv[1]
+    text = cd.paste()
 
-to_lang = sys.argv[1]
-
-
-text = cd.paste()
-
-try:
     output = get_translate(text=text, to_lang=to_lang, from_lang="AUTO")
     cd.copy(output)
-except Exception:
-    print("Sorry, try again")
